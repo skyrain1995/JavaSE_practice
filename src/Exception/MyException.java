7a1061939187b4bd6887e6b0a40a4b018013bc6b
skyrain1995 <2503123003@qq.com>
2019-01-27 11:50:51 +0800

    第一次异常处理，带finally

---
+++ src/Exception/MyException.java
@@ -0,0 +1,24 @@
+package Exception;
+
+/**
+ * 自定义异常处理类,message为异常的描述信息
+ */
+public class MyException extends Exception{
+    public MyException(){}
+
+    public MyException(String message){
+        super(message);
+    }
+}
+//测试自定义类
+class TestMyException{
+    void test() throws MyException{}
+
+    public static void main(String[] args) {
+        try {
+            new TestMyException().test();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+    }
+}
