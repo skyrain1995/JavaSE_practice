f99531959f1b26e6257eee0d9807f3426f5edb83
skyrain1995 <2503123003@qq.com>
2019-01-28 21:14:00 +0800

    字符集测试之编码

--- src/IO/Encoding.java
+++ src/IO/Encoding.java
@@ -1,13 +1,20 @@
 package IO;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * 编码：字符串->字节
  */
 public class Encoding {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         String msg = "今天天气好a";
         //编码：字节数组
         byte[] datas = msg.getBytes();
-        System.out.println(datas.length);
+        System.out.println(datas.length);  //UTF-8中文3字节，英文1字节
+
+        //编码：其他字符集
+        datas = msg.getBytes("GBK");
+        System.out.println(datas.length);   //GBK中文2个字节，英文1字节
+
     }
 }
