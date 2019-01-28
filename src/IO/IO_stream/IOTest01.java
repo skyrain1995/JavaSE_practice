e45603937b64b82c0e8b2bbeb7102843f6db3f4e
skyrain1995 <2503123003@qq.com>
2019-01-28 22:21:54 +0800

    IO流初步操作（理解步骤）

---
+++ src/IO/IO_stream/IOTest01.java
@@ -0,0 +1,31 @@
+package IO.IO_stream;
+
+import java.io.*;
+
+/**
+ * 理解操作步骤
+ * 1.创建源 2.选择流 3.操作（读/写） 4.释放
+ */
+public class IOTest01 {
+    public static void main(String[] args) {
+        //1.创建源
+        File src = new File("src/IO/abc.txt");
+        //2.选择流
+        try {
+            InputStream is = new FileInputStream(src);
+            //3.操作(读取)
+            int data1 = is.read();
+            int data2 = is.read();
+            System.out.println((char)data1);  //第一个数据h
+            System.out.println((char)data2);  //第二个数据e。。。最后读到不是数据，文件末尾返回-1
+            //4.释放
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
