bad09b23804bc43ba4cd4655c4b79a823925ee67
skyrain1995 <2503123003@qq.com>
2019-01-31 16:27:58 +0800

    测试执行SQL语句以及SQL注入问题

---
+++ src/jdbc/Demo02.java
@@ -0,0 +1,32 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 测试执行SQL语句以及SQL注入问题
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        try {
+            //加载驱动类
+            Class.forName("com.mysql.jdbc.Driver");
+            //建立连接
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjdbc","root","root");
+            Statement stmt = conn.createStatement();
+//            String sql = "insert into t_user(username,pwd,regtime) values ('王五',555,now())";
+//            stmt.execute(sql);
+
+            //测试SQL注入，为了避免，一般不使用Statement
+//            String id = "5 or 1=1";     //SQL注入，在传入数据库的部分可能有恶意代码，这句一执行，数据库表被删掉
+//           String sql = "delete from t_user where id = "+id;
+//            stmt.execute(sql);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
