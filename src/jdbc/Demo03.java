33d2ea0c9ec7c952dec8a1559cc95469e0f83c31
skyrain1995 <2503123003@qq.com>
2019-01-31 18:49:21 +0800

    测试PreparedStatement的基本用法

--- src/jdbc/Demo03.java
+++ src/jdbc/Demo03.java
@@ -1,9 +1,6 @@
 package jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * 测试PreparedStatement的基本用法
@@ -11,14 +8,16 @@ import java.sql.SQLException;
  */
 public class Demo03 {
     public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
         try {
             //加载驱动类
             Class.forName("com.mysql.jdbc.Driver");
             //建立连接
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjdbc","root","root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjdbc","root","root");
             //? 占位符，可以避免sql注入
             String sql = "insert into t_user(username,pwd,regtime) values (?,?,now())";
-            PreparedStatement ps = conn.prepareStatement(sql);
+            ps = conn.prepareStatement(sql);
             ps.setString(1,"王六"); //参数索引是从1开始计算，而不是0
             ps.setString(2,"789");  //setObject()方法比较通用
             ps.execute();
@@ -27,6 +26,21 @@ public class Demo03 {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if(ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
