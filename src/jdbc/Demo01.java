def10494771a36a8af8322b518e9bea082b977ce
skyrain1995 <2503123003@qq.com>
2019-01-31 18:43:48 +0800

    测试和数据库建立连接

--- src/jdbc/Demo01.java
+++ src/jdbc/Demo01.java
@@ -9,17 +9,26 @@ import java.sql.SQLException;
  */
 public class Demo01 {
     public static void main(String[] args) {
+        Connection conn = null;
         try {
             //加载驱动类
             Class.forName("com.mysql.jdbc.Driver");
             //建立连接(连接对象内部其实包含了Socket对象，是一个远程的连接，比较耗时)
             //真正开发中，为了提高效率，都会通过连接池来管理连接对象
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjdbc","root","root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjdbc","root","root");
             System.out.println(conn);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
